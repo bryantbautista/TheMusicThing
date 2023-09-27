fbdbd8f032e1c4e90c0fe611a6a045276ce54957
Caden King <ivashinking@gmail.com>
2023-09-26 18:34:57 -0700

    Hello World 2

--- django_projects/MusicThing/urls.py
+++ django_projects/MusicThing/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("helloworld2", views.helloworld2, name="helloworld2")
 ]
--- django_projects/MusicThing/views.py
+++ django_projects/MusicThing/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world")
+    return HttpResponse("Hello, world!")
+def helloworld2(request):
+    return HttpResponse("Hello, world 2!")
