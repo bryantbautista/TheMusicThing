70b05a68efe91bcde5eb86d92c8286402f62e1eb
Caden King <ivashinking@gmail.com>
2023-10-25 23:02:18 -0700

    Added logout functionality

--- django_projects/MusicThing/urls.py
+++ django_projects/MusicThing/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("helloworld2", views.helloworld2, name="helloworld2"),
-    path("accounts/", include("django.contrib.auth.urls"))
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/logout", views.logoutaccount, name="logout")
 ]
--- django_projects/MusicThing/views.py
+++ django_projects/MusicThing/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import logout
 from django import forms
+from forms import RegistrationForm
 
 
 # Create your views here.
@@ -10,5 +12,10 @@ def index(request):
         'test_var':1
     }
     return render(request, 'index.html', context)
+
 def helloworld2(request):
     return HttpResponse("Hello, world 2!")
+
+def logoutaccount(request):
+    logout(request)
+    return HttpResponseRedirect('/')
