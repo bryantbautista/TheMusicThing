0423978b972245847f546ed853c761bec37f7938
Bryant Bautista <bbautistabryant@gmail.com>
2023-11-04 17:53:15 -0700

    initial linking of homePage

--- django_projects/MusicThing/urls.py
+++ django_projects/MusicThing/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("logout", views.logoutView, name="logout"),
     path("login", views.loginView, name="login"),
-    path("register", views.registerView, name="register")
+    path("register", views.registerView, name="register"),
+    path("home", views.homeView, name="home")
 ]
