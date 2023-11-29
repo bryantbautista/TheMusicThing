9f9a005560de50072663f3e06e24045167ec513e
Caden King <caden.s.king@sjsu.edu>
2023-11-28 17:46:53 -0800

    Finished Scuffed Backend, Finished Frontend

--- django_projects/MusicThing/views.py
+++ django_projects/MusicThing/views.py
@@ -179,9 +179,11 @@ def chartsView(request):
         id_count[rating.AlbumID] = id_count.get(rating.AlbumID, 0) + 1
 
     id_avg = {}
+    id_avg2 = {}
 
     for id, sum in id_sum.items():
         id_avg[id] = round(sum / id_count[id], 2)
+        id_avg2[id] = round(sum / id_count[id], 2)
 
     topAlbums = []
 
@@ -197,16 +199,20 @@ def chartsView(request):
         params = {
             'ids': ','.join(topAlbums)
         }
-
-
         req = urllib.request.Request('https://api.spotify.com/v1/albums' + '?' + urllib.parse.urlencode(params))
         req.add_header('Authorization', 'Bearer ' + token)
         req.add_header('Accept', 'application/json')
         try:
             response = json.loads(urllib.request.urlopen(req).read().decode('utf-8'))
         except:
-            return HttpResponse("Album not found.")
+            return HttpResponse("Error: Album not found.")
         
-        for album in response['albums']:
-            print(album['name'])
-    return render(request, "charts.html")
+        albumInfo = []
+        print(id_count[response['albums'][0]['id']])
+        for i in range(0, len(response['albums'])):
+            albumInfo.append({'rank': i+1, 'img': response['albums'][i]['images'][0]['url'], 
+                              'title': response['albums'][i]['name'], 
+                              'artist': response['albums'][i]['artists'][0]['name'], 
+                              'avgrating': id_avg2[response['albums'][i]['id']], 
+                              'totalratings': id_count[response['albums'][i]['id']]})
+    return render(request, "charts.html", {'albumInfo': albumInfo})
